8e7e7c3a5f17abdb886784a9f922f97c509d3cca
YokoyamaRicha <151817252+YokoyamaRicha@users.noreply.github.com>
2023-11-25 17:46:58 +0900

    Update activemotion.py

--- activemotion.py
+++ activemotion.py
@@ -11,12 +11,16 @@ s.connect((HOST, PORT))
 def toBytes(str):
     return bytes(str.encode())
 
+def active():
+    
+
 def main():
     b=0
-    while b<3:
+    while True:
         s.send(toBytes("movej([-3.63,-2.04,1.58,-1.73,-1.66,1.04], a=0.50, v=0.50)"+"\n"))
         time.sleep(4)
-        b+=1
+        active()
+        
 
 
 if __name__ == '__main__':
