7ed59b1405fd55217409cc5ccd2412537eb62516
YokoyamaRicha <151817252+YokoyamaRicha@users.noreply.github.com>
2023-11-27 16:43:54 +0900

    Update random.py

--- random.py
+++ random.py
@@ -30,8 +30,24 @@ g = gripper.RobotiqGripper()
 g.connect(HOST, 63352)
 g.activate()
 
-a=random.randint(-100, 100)+random.randint(0,9)*0.1
-print(a)
+def main():
+  while True:
+    inspect()
+    active()
+    active()
+    
+
+def inspect():
+    s.send(toBytes("movej([-3.63,-2.04,1.58,-1.73,-1.66,1.04], a=0.50, v=0.50)"+"\n"))
+    time.sleep(4)
+
+def active():
+  a=random.randint(-3, 3)+random.randint(0,9)*0.1
+  b=random.randint(-3, 3)+random.randint(0,9)*0.1
+  print("[-3.63,-2.04,1.58,-1.73"+str(a)+str(b)+"]")
+  s.send(toBytes("movej([-3.63,-2.04,1.58,-1.73,a,b], a=0.50, v=0.50)"+"\n"))
+  time.sleep(3)
+  
 
 
 
@@ -54,4 +70,10 @@ print(a)
 
 
 
+
+
+
+if __name__ == '__main__':
+    main()
+    s.close()
 
